c63c7c3362630f7093bdf3035aae2daf8aa69b81
wardonne <Wardonne@yandex.ru>
2023-12-19 20:41:36 +0400

    Initial Commit

---
+++ AISD_2LAB/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AISD_2LAB
+//
+//  Created by Роман Казаев on 19.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
