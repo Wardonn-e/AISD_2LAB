349601e5bf02da28bf52e5adcfbb6d8ab2156c2d
Wardonn-e <118371995+Wardonn-e@users.noreply.github.com>
2023-12-25 22:41:41 +0400

    final

--- AISD_2LAB/main.cpp
+++ AISD_2LAB/main.cpp
@@ -2,7 +2,16 @@
 #include <cstdlib>
 
 using namespace std;
+template<typename T>
+struct Term {
+    T coefficient;
+    int exponent;
 
+    Term(T coeff, int exp) {
+        coefficient = coeff;
+        exponent = exp;
+    }
+};
 template<typename T>
 struct Node {
     Node<T>* _next;
@@ -14,13 +23,31 @@ template<typename T>
 class LinkedList {
 private:
     Node<T>* _head;
+    void push(const T& data, int in_head){
+        Node<T>* new_head = new Node<T>(data);
+        if (!_head) {
+            _head = new_head;
+            _head->_next = _head;
+            return;
+        }
+        Node<T>* temp = _head;
+        while (temp->_next != _head)
+        {
+            temp = temp->_next;
+        }
+        temp->_next = new_head;
+        new_head->_next = _head;
+        if (in_head){
+            _head = new_head;
+        }
+    }
 public:
     LinkedList() : _head(nullptr) {}
     
     
     LinkedList(size_t size, T min_value, T max_value) : _head(nullptr) {
         for (size_t i = 0; i < size; ++i) {
-            T random_value = static_cast<T>(rand() % (max_value - min_value + 1) + min_value);
+            T random_value = (rand() % (max_value - min_value + 1) + min_value);
             push_tail(random_value);
         }
     }
@@ -32,7 +59,6 @@ public:
                 ptr = ptr->_next;
                 delete temp;
             } while (ptr && ptr != _head);
-
             _head = nullptr;
         }
     }
@@ -67,42 +93,31 @@ public:
         } while (ptr != _head);
         return size;
     }
+    
+    void push_head(const int& data) {
+        push(data, 1);
+    }
+    void push_tail(const int& data) {
+        push(data, 0);
+    }
+    
+    void push_head(const Term<int>& data) {
+        if (data.coefficient != 0) push(data, 1);
+    }
+    
+    void push_tail(const Term<int>& data) {
+        if (data.coefficient != 0) push(data, 0);
+    }
+    
+   
+    
+    void push_head(const LinkedList& a) {
+        LinkedList temp(a);
+        temp.push_tail(*this);
+        swap(temp);
+        
+    }
 
-    void push_head(const T& data) {
-        Node<T>* new_head = new Node<T>(data);
-        if (!_head) {
-            _head = new_head;
-            _head->_next = _head;
-            return;
-        }
-        Node<T>* temp = _head;
-        while (temp->_next != _head)
-        {
-            temp = temp->_next;
-        }
-        temp->_next = new_head;
-        new_head->_next = _head;
-        _head = new_head;
-    }
-    void push_tail(const T& data) {
-        Node<T>* new_head = new Node<T>(data);
-        if (!_head) {
-            _head = new_head;
-            _head->_next = _head;
-            return;
-        }
-        Node<T>* temp = _head;
-        while (temp->_next != _head)
-        {
-            temp = temp->_next;
-        }
-        temp->_next = new_head;
-        new_head->_next = _head;
-    }
-    void push_head(LinkedList a) {
-        a.push_tail(*this);
-        swap(a);
-    }
 
     void push_tail(const LinkedList& a) {
         Node<T>* ptr = a._head;
@@ -111,6 +126,7 @@ public:
             ptr = ptr->_next;
         } while (ptr != a._head);
     }
+    
 
     ostream& print(ostream& os = cout) const {
         Node<T>* ptr = _head;
@@ -125,9 +141,37 @@ public:
         return os;
     }
 
-    const T& operator[](size_t index) const {
+    // Метод для вывода узлов типа Term
+    void printTerms() const {
         Node<T>* ptr = _head;
-        for (size_t i = 0; i < index; i++) {
+        Node<T>* ptr_head = _head;
+        do {
+            std::cout << ptr->_data.coefficient << "x^" << ptr->_data.exponent;
+            ptr = ptr->_next;
+            if (ptr != ptr_head) {
+                std::cout << " + ";
+            }
+        } while (ptr != ptr_head);
+        std::cout << std::endl;
+    }
+    
+    double evaluate(double x) const {
+        double result = 0;
+        Node<T>* ptr = _head;
+        Node<T>* ptr_aisd = _head;
+        do {
+            result += ptr->_data.coefficient * pow(x, ptr->_data.exponent);
+            ptr = ptr->_next;
+        } while(ptr != ptr_aisd);
+        return result;
+    }
+    
+    const T& operator[](int index) const {
+        if (index < 0) {
+            throw std::out_of_range("Negative index");
+        }
+        Node<T>* ptr = _head;
+        for (int i = 0; i < index; i++) {
             ptr = ptr->_next;
             if (ptr == _head) {
                 throw std::out_of_range("Index out of range");
@@ -136,9 +180,12 @@ public:
         return ptr->_data;
     }
 
-    T& operator[](size_t index) {
+    T& operator[](int index) {
+        if (index < 0) {
+            throw std::out_of_range("Negative index");
+        }
         Node<T>* ptr = _head;
-        for (size_t i = 0; i < index; i++) {
+        for (int i = 0; i < index; i++) {
             ptr = ptr->_next;
             if (ptr == _head) {
                 throw std::out_of_range("Index out of range");
@@ -153,7 +200,6 @@ public:
         }
         if (_head->_next == _head) {
             delete _head;
-            _head = nullptr;
             return;
         }
         Node<T>* ptr = _head;
@@ -185,141 +231,107 @@ public:
     }
 
     void delete_node(const T& data) {
-        Node<T>* ptr = _head;
-        Node<T>* ptr1 = nullptr;
+        if (!_head) {
+            return;
+        }
+
+        Node<T>* current = _head;
+        Node<T>* previous = nullptr;
+
         do {
-            if (ptr->_data == data) {
-                if (ptr == _head) {
-                    if (ptr->_next == _head) {
-                        delete ptr;
+            if (current->_data == data) {
+                if (current == _head) {
+                    // Удаляемый узел - первый в списке
+                    if (current->_next == _head) {
+                        // Список состоит только из одного элемента
+                        delete current;
+                        _head = nullptr;
                         return;
                     }
-                    Node<T>* ptr_last = _head;
-                    while (ptr_last->_next != _head) {
-                        ptr_last = ptr_last->_next;
-                    }
-                    _head = ptr->_next;
-                    ptr_last->_next = ptr->_next;
-                    Node<T>* temp = ptr;
-                    ptr = ptr->_next;
-                    delete temp;
+
+                    // Обновляем указатель на голову
+                    _head = current->_next;
+                } else {
+                    // Удаляемый узел не первый в списке
+                    previous->_next = current->_next;
                 }
-                else {
-                    ptr1->_next = ptr->_next;
-                    Node<T>* temp = ptr;
-                    ptr = ptr->_next;
-                    delete temp;
+
+                // Удаляем текущий узел
+                Node<T>* temp = current;
+                current = current->_next;
+                delete temp;
+
+                // Если узел был последним, обновляем указатель на голову
+                if (current == _head) {
+                    _head = previous->_next;
                 }
+            } else {
+                // Переходим к следующему узлу
+                previous = current;
+                current = current->_next;
             }
-            else {
-                ptr1 = ptr;
-                ptr = ptr->_next;
-            }
-        } while (ptr != _head);
+        } while (current != _head);
     }
+
 
     Node<T>* get_head() const {
         return _head;
     }
 };
 
-template<typename T>
-struct Term {
-    T coefficient;
-    int exponent;
-    Term(T coeff, int exp) : coefficient(coeff), exponent(exp) {}
-};
-
-template<typename T>
-class Polynomial {
-private:
-    LinkedList<Term<T>> terms;
-
-public:
-    Polynomial() {}
-
-    void addTerm(const T& coefficient, int exponent) {
-        if (coefficient != 0) {
-            terms.push_tail(Term<T>(coefficient, exponent));
-        }
-    }
-
-    T evaluate(T x) const {
-        T result = 0;
-        Node<Term<T>>* ptr = terms.get_head();
-        Node<Term<T>>* ptr_aisd = terms.get_head();
-        do {
-            result += ptr->_data.coefficient * pow(x, ptr->_data.exponent);
-            ptr = ptr->_next;
-        } while(ptr != ptr_aisd);
-        return result;
-    }
-
-    void print() const {
-        Node<Term<T>>* ptr = terms.get_head();
-        Node<Term<T>>* ptr_head = terms.get_head();
-        do {
-            std::cout << ptr->_data.coefficient << "x^" << ptr->_data.exponent;
-            ptr = ptr->_next;
-            if (ptr != ptr_head) {
-                std::cout << " + ";
-            }
-        } while (ptr != ptr_head);
-        std::cout << std::endl;
-    }
-};
-
-
 int main() {
-    Polynomial<double> poly;
-    poly.addTerm(2.0, 3);
-    poly.addTerm(-1.5, 2);
-    poly.addTerm(1.0, 1);
-    poly.addTerm(4.5, 0);
+    // Создаем пустой список
+    LinkedList<int> list1;
 
-    poly.print();
+    // Добавляем элементы в конец списка
+    list1.push_tail(1);
+    list1.push_tail(2);
+    list1.push_tail(3);
 
-    double x = 2.0;
-    double result = poly.evaluate(x);
-
-    std::cout << "x = " << x << " is: " << result << std::endl;
-
-    
-    
-    
-    
-    
-    LinkedList<int> list;
-
-    list.push_head(3);
-    list.push_head(2);
-    list.push_head(1);
-
-    list.push_tail(4);
-    list.push_tail(5);
-    list.push_tail(6);
-
-    cout << "list: ";
-    list.print();
+    // Выводим список на экран
+    cout << "List 1: ";
+    list1.print();
     cout << endl;
 
-    Node<int>* head = list.get_head();
-    if (head) {
-        cout << "Head value: " << head->_data << endl;
-    }
+    // Создаем список с рандомными значениями
+    LinkedList<int> list2(5, 10, 20);
 
-    cout << "Element at index 2: " << list[2] << endl;
+    // Выводим список на экран
+    cout << "List 2: ";
+    list2.print();
+    cout << endl;
 
-    list.pop_head();
-    list.pop_tail();
+    // Создаем список из термов
+    LinkedList<Term<int>> list3;
+    list3.push_tail(Term<int>(2, 3));
+    list3.push_tail(Term<int>(-1, 2));
+    list3.push_tail(Term<int>(4, 0));
+    list3.push_tail(Term<int>(0, 2)); // проверяем, можно ли добавить Term с коэф = 0
 
-    list.delete_node(3);
+    // Выводим список термов на экран
+    cout << "List 3 (Terms): ";
+    list3.printTerms();
+    cout << endl;
 
-    cout << "Modified list: ";
-    list.print();
+    // Вычисляем значение многочлена для x=2
+    double result = list3.evaluate(2);
+    cout << "Result of evaluating List 3 at x=2: " << result << endl;
+
+    // Добавляем один список в конец другого
+    list1.push_tail(list2);
+
+    // Выводим объединенный список на экран
+    cout << "Combined List 1 after adding List 2: ";
+    list1.print();
+    cout << endl;
+
+    // Удаляем первый элемент из списка
+    list1.pop_head();
+
+    // Выводим список после удаления первого элемента
+    cout << "List 1 after popping head: ";
+    list1.print();
     cout << endl;
 
     return 0;
-    
 }
-
